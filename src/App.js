f2ca44bcba80555186ad03cc9738119272554ffb
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-04-17 00:01:19 +0600

    initialize Firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-
+import logo from './logo.svg';
 import './App.css';
-import Question from './Component/Question/Question';
-
-
-import Shop from './Component/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='header-name'>Boi-Ghor</h1>
-      <p className='content'>Choose Your Books</p>
-      <Shop></Shop>
-      <Question></Question>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
 
 export default App;
